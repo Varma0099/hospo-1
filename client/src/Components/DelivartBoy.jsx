1715d1ae90ac8de91f18a97f72a1d50df4a4577a
Mohana Vamsi <mohanavamsi14@gmail.com>
2024-10-28 10:34:31 +0530

    Update DelivartBoy.jsx

--- client/src/Components/DelivartBoy.jsx
+++ client/src/Components/DelivartBoy.jsx
@@ -7,7 +7,7 @@ function DeliveryBoy() {
     const [data,setdata]=useState({lat:"",lng:""})
 
     useEffect(() => {
-        const socketIo = io('https://hospo.onrender.com');
+        const socketIo = io('https://hospo-164o.onrender.com');
         socketIo.on("customerloc",(log,lat)=>{
             setdata({lat:lat,lng:log})
         })
